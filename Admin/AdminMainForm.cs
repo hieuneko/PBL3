bb9990a62e92612e556309d4e56d0daadb40c068
hieuneko <hieuneko@gmail.com>
2021-05-11 20:49:10 +0700

    Them class cbbitem, cac phuong thuc getlist, show, search

--- Admin/AdminMainForm.cs
+++ Admin/AdminMainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
+using DTO;
 
 namespace Admin
 {
@@ -15,6 +17,32 @@ namespace Admin
         public AdminMainForm()
         {
             InitializeComponent();
+            setCBBItem();
         }
+        public void setCBBItem()
+        {
+            comboBox2.Items.AddRange(BusinessLogicLayer.Instance.GetCBBItems().ToArray());
+        }
+        public void ShowList(string st)
+        {
+            dataGridView1.DataSource = null;
+            if (comboBox2.SelectedIndex == -1) MessageBox.Show("Chon Danh Muc");
+            else
+            {
+                int ID = ((CBBItem)comboBox2.SelectedItem).IdDanhMuc;
+                dataGridView2.DataSource = BusinessLogicLayer.Instance.getListMonBySearch_BLL(ID,st);
+                this.dataGridView1.Columns[0].Visible = false;
+            }
+        }
+        private void button_Show(object sender, EventArgs e)
+        {
+            ShowList("");
+        }
+        private void button_Search(object sender, EventArgs e)
+        {
+            string st = textBox2.Text;
+            ShowList(st);
+        }
+
     }
 }
---
+++ DTO/CBBItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CBBItem
+    {
+        public int IdDanhMuc { get; set; }
+        public string DanhMuc { get; set; }
+        public override string ToString()
+        {
+            return DanhMuc;
+        }
+    }
+}
